900f6fc55994ef68a7932986cddb2cb6a4dd8cf5
Ahmad Nawaz <106543558+ahmad-an@users.noreply.github.com>
2023-04-09 11:09:35 +0500

    added day5 folder

---
+++ Javascript Online Course/Day 5/app.js
@@ -0,0 +1,32 @@
+// Arrays in Javascript
+// Array is used to store multiple values of same types in a single variable
+// Javascript allows to store values of different types in same array but it is not recommended to do so
+
+let months = ["January", "February", "March"];
+
+// to view array in console
+console.log(months);
+
+
+// to view array values by index
+
+console.log(months[0]);     // Arrays store values in index starting from 0 (zero) index 
+console.log(months[2]);     // To view value of array at index 2;  Result: March
+
+// To view value of array at index which is not available in array will result: Undefined
+console.log(months[12]);
+
+
+// Adding Values in Array
+// Data or values can be added in an existing arry by three methods, 
+
+months[3] = "April";    // Data can be stored in arry by using index number, but it is not recommended way;
+console.log(months);    // Varify the result of adding data by using index number, Result: ['January', 'February', 'March', 'April']
+console.log(months[3]); // View data at index number 3, to varify the result; // April
+
+// Adding data in array by using Push method
+
+months.push("May");     // Push method add data in array at the end after last index of array;
+console.log(months);    // varify result;
+console.log(months[4]); // variey result by index number i.e. 4 in this case;
+
