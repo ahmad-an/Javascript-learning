41e642361764d515115596531f85a7a62871790d
Ahmad Nawaz <106543558+ahmad-an@users.noreply.github.com>
2023-04-09 11:32:29 +0500

    adding data in array by unshift

--- Javascript Online Course/Day 5/app.js
+++ Javascript Online Course/Day 5/app.js
@@ -17,22 +17,39 @@ console.log(months[2]);     // To view value of array at index 2;  Result: March
 console.log(months[12]);
 
 
-// Adding Values in Array
-// Data or values can be added in an existing arry by three methods, 
+// Adding Values in Array;
+// Data or values can be added in an existing arry by three methods; index number, push method, unshift method;
 
-months[3] = "April";    // Data can be stored in arry by using index number, but it is not recommended way;
-console.log(months);    // Varify the result of adding data by using index number, Result: ['January', 'February', 'March', 'April']
-console.log(months[3]); // View data at index number 3, to varify the result; // April
+    // Adding data in array by index number;
 
-// Adding data in array by using Push method
+        months[3] = "April";    // Data can be stored in arry by using index number, but it is not recommended way;
+        console.log(months);    // Varify the result of adding data by using index number, Result: ['January', 'February', 'March', 'April']
+        console.log(months[3]); // View data at index number 3, to varify the result; // April
 
-months.push("May");     // Push method add data in array at the end after last index of array;
-console.log(months);    // varify result;
-console.log(months[4]); // variey result by index number i.e. 4 in this case;
+    // Adding data in array by using push method;
 
-// multiple values can also be added through single push method;
-months.push("Jun", "July", "August");   // adding multiple values in single push;
-console.log(months);                    // varify multiple value added via single push;
-console.log(months[5]);                 // varify by index number
-console.log(months[6]);
-console.log(months[7]);
+        months.push("May");     // push method add data in array at the end after last index of array;
+        console.log(months);    // varify result;
+        console.log(months[4]); // variey result by index number i.e. 4 in this case;
+
+        // multiple values can also be added through single push method;
+        
+        months.push("Jun", "July", "August");   // adding multiple values in single push;
+        console.log(months);                    // varify multiple value added via single push;
+        console.log(months[5]);                 // varify by index number
+        console.log(months[6]);
+        console.log(months[7]);
+
+    // Adding data in array by using unshift metho
+
+        months.unshift("September");            // unshift method add data in existing array at the start of array at index 0,
+        console.log(months);                    // varify the data added by unshift;
+        console.log(months[0]);                 // varify the data added by unshift at index 0;
+
+        // multiple values can also be added through single unshift method;
+
+        months.unshift("October", "November", "December");  // adding mulitple values via single unshift;
+        console.log(months);                    // varify the multiple data added via single unshift;
+        console.log(months[0]);                 // varify the data by index number
+        console.log(months[1]);
+        console.log(months[2]);
