36e7717632001dd4bdcd863117cf5a4b33afb34c
Ahmad Nawaz <106543558+ahmad-an@users.noreply.github.com>
2023-04-11 22:30:43 +0500

    JS assignment 2 Q3 solution modified

--- Javascript Online Course/Day 6/Assignment/Question 3/app.js
+++ Javascript Online Course/Day 6/Assignment/Question 3/app.js
@@ -6,4 +6,19 @@
 function changePara(fieldId){
     para = "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Est, at. Atque mollitia illo enim aspernatur, quo eligendi fugit architecto. Alias aliquid quaerat eaque qui possimus porro atque, aliquam vel quibusdam."
     document.getElementById("para").innerText = para;
+    document.getElementById("para").style.position = "bottom";
 }
+
+
+
+function addParagraph(para) {
+    let parag = document.createElement("p");
+    parag.textContent = para;
+    document.body.appendChild(parag);
+}
+
+
+let para = "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Est, at. Atque mollitia illo enim aspernatur, quo eligendi fugit architecto. Alias aliquid quaerat eaque qui possimus porro atque, aliquam vel quibusdam.";
+addParagraph(para);
+
+
